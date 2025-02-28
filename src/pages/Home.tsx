bc7ea192707d788a35a7c3d7022d04ad05d17459
MohamDahALU <m.dahab@alustudent.com>
2025-02-28 17:05:28 +0200

    remove i18n

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,20 @@
 import { useNavigate } from "react-router-dom"
 import logo from "../assets/logo.png"
-import { useTranslation } from "react-i18next"
 
 
 export default function Home() {
   const navigate = useNavigate()
-  const {t} = useTranslation()
 
   return (
     <>
-      <h1 className="my-20 text-primary-purple font-bold">{t("greeting")}</h1>
+      <h1 className="my-20 text-primary-purple font-bold">WELCOME</h1>
       <img className="w-1/4 max-w-40"
         src={logo} alt="Logo" />
 
       <button className="rect bg-secondary-purple text-primary-purple mt-14"
-        >Join Queue</button>
+      >Join Queue</button>
       <button className="rect bg-primary-purple text-white mt-6"
-      onClick={() => navigate("/create")}>Setup Queue</button>
+        onClick={() => navigate("/create")}>Setup Queue</button>
     </>
   )
 }
